49fefc92af44b9730dbe1abd8c475a39b2a0cce3
Karik <kl.duszak@gmail.com>
2024-07-29 19:35:21 +0200

    DeleteMatchDynamicInformationById domain implemented

---
+++ lib/features/mentor_center/domain/usecases/delete_match_dynamic_information_by_id.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../repositories/match_dynamics_repository.dart';
+
+class DeleteMatchDynamicInformationById extends UseCase<int, Params>{
+  final MatchDynamicRepository repository;
+  
+  DeleteMatchDynamicInformationById(this.repository);
+
+  @override
+  Future<Either<Failure, int>> call(Params params) async {
+    return await repository.deleteMatchDynamicsInformationById(params.id);
+  }
+}
+
+class Params extends Equatable{
+  final int id;
+  
+  Params({
+    required this.id
+  });
+
+  @override
+  List<Object?> get props => [id];
+  
+}
---
+++ test/features/mentor_center/domain/usecases/delete_match_dynamics_informations_by_id_test.dart
@@ -0,0 +1,42 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:virtual_tennis_mentor/features/mentor_center/domain/repositories/match_dynamics_repository.dart';
+import 'package:virtual_tennis_mentor/features/mentor_center/domain/usecases/delete_match_dynamic_information_by_id.dart';
+
+class MockMatchDynamicRepository extends Mock
+    implements MatchDynamicRepository {}
+
+
+void main(){
+  late DeleteMatchDynamicInformationById usecase;
+  late MockMatchDynamicRepository mockMatchDynamicRepository;
+
+  setUp((){
+    mockMatchDynamicRepository = MockMatchDynamicRepository();
+    usecase = DeleteMatchDynamicInformationById(mockMatchDynamicRepository);
+  });
+
+  int tSuccessExitCode = 200;
+  int tId = 10;
+
+  test(
+    'Should obtain success exit code',
+    () async {
+      // arrange
+
+      // when is "On the fly" implementation return Right(tAllMatchDynamicInformations)
+      // any time getAllMatchDynamicInformations is called
+      when(() => mockMatchDynamicRepository.deleteMatchDynamicsInformationById(tId))
+        .thenAnswer((_) async => Right(tSuccessExitCode));
+
+      // act
+      final result = await usecase(Params(id: tId));
+
+      // assert
+      expect(result, Right(tSuccessExitCode));
+      verify(() => mockMatchDynamicRepository.deleteMatchDynamicsInformationById(tId));
+      verifyNoMoreInteractions(mockMatchDynamicRepository);
+    }
+  );
+}
