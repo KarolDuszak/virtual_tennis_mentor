9cadf9c166915113b793fc9e31fc7ad03a7de50e
Karik <kl.duszak@gmail.com>
2024-07-31 21:11:39 +0200

    tests for preferences prepared

--- lib/core/config/preferences.dart
+++ lib/core/config/preferences.dart
@@ -14,12 +14,21 @@ class UserPreferencesImpl implements UserPreferences {
   UserPreferencesImpl(this.customIo, this.sharedPreferences);
 
   @override
-  Future<String> get language {
+  Future<String> get language async {
+    String jsonString = 'default';
+    try {
+      jsonString = sharedPreferences.getString('language') ?? 'deafult';
+    } catch (_) {}
+    //final decodedString = jsonString.
     final systemLanguage = customIo.getLocaleName();
     //it should use default device language in not found in preferences
     //but enable to change it and store in app preferences
     //if(sharedPreferences.language != 'default' && null && len!=2 && language contains UperCase) then this below
-    //return sharedPreferences.language;
+    if (jsonString != 'default' &&
+        jsonString.length == 2 &&
+        jsonString == jsonString.toLowerCase()) {
+      return Future.value(jsonString);
+    }
 
     return Future.value(systemLanguage.substring(0, 2).toLowerCase());
   }
--- test/core/config/preferences_test.dart
+++ test/core/config/preferences_test.dart
@@ -1,9 +1,13 @@
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:virtual_tennis_mentor/core/config/preferences.dart';
 import 'package:virtual_tennis_mentor/core/platform/custom_io.dart';
+
+import '../../fixtures/fixture_reader.dart';
 
 class MockCustomIo extends Mock implements CustomIo {}
 
@@ -27,7 +31,8 @@ void main() {
         'should return response length eaqule first 2 when preferences set to default',
         () async {
           // arrange
-          //when(() => mockSharedPreferences).thenReturn("default")
+          when(() => mockSharedPreferences.getString('language'))
+              .thenReturn(fixture('user_preferences_default.json'));
           when(() => mockCustomIo.getLocaleName()).thenReturn("long_response");
           // act
           final result = await userPreferences.language;
@@ -42,10 +47,13 @@ void main() {
         'should return language from shared preferences if not set to default',
         () async {
           // arrange
-
+          when(() => mockSharedPreferences.getString('language'))
+              .thenReturn(fixture('user_preferences.json'));
+          when(() => mockCustomIo.getLocaleName()).thenReturn("long_response");
           // act
-
+          final result = await userPreferences.language;
           // assert
+          expect(result, 'pl');
         },
       );
 
@@ -53,10 +61,29 @@ void main() {
         'should default if language stored in preferences is not len(2) and lower case',
         () async {
           // arrange
-
+          when(() => mockSharedPreferences.getString('language'))
+              .thenReturn(fixture('bad_user_preferences.json'));
+          when(() => mockCustomIo.getLocaleName()).thenReturn("en_UK");
           // act
-
+          final result = await userPreferences.language;
           // assert
+          expect(result.length, 2);
+          expect(result, 'en');
+        },
+      );
+
+      test(
+        'should return default on get shared preferences exception',
+        () async {
+          // arrange
+          when(() => mockSharedPreferences.getString('language'))
+              .thenThrow(Exception());
+          when(() => mockCustomIo.getLocaleName()).thenReturn("en_UK");
+          // act
+          final result = await userPreferences.language;
+          // assert
+          expect(result.length, 2);
+          expect(result, 'en');
         },
       );
 
@@ -68,6 +95,7 @@ void main() {
           // act
 
           // assert
+          expect(true, false);
         },
       );
     },
