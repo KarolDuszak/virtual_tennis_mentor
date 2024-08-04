c955e0fab423407bc9017c75d918ebf965ac838d
Karik <kl.duszak@gmail.com>
2024-08-04 11:17:18 +0200

    test for injecting database pass

--- lib/features/mentor_center/data/datasources/match_dynamics_local_datasource.dart
+++ lib/features/mentor_center/data/datasources/match_dynamics_local_datasource.dart
@@ -1,3 +1,9 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
+import 'package:sqflite/sqflite.dart';
+
 import '../models/match_dynamics_model.dart';
 
 abstract class MatchDynamicsLocalDataSource {
@@ -12,4 +18,63 @@ abstract class MatchDynamicsLocalDataSource {
       MatchDynamicsModel matchDynamic);
 
   Future<MatchDynamicsModel> getMatchDynamicsInfoById(int id);
+}
+
+class MatchDynamicsLocalDatasourceImpl implements MatchDynamicsLocalDataSource {
+  static final MatchDynamicsLocalDatasourceImpl instance =
+      MatchDynamicsLocalDatasourceImpl._internal();
+
+  static Database? _database;
+
+  MatchDynamicsLocalDatasourceImpl._internal();
+
+  MatchDynamicsLocalDatasourceImpl.injectDatabase(Database db) {
+    _database = db;
+  }
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database!;
+    }
+
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    //TODO: Load database from assets
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> deleteMatchDynamicsInfoById(int id) {
+    // TODO: implement deleteMatchDynamicsInfoById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<MatchDynamicsModel>> getAllMatchDynamicsInfo() {
+    // TODO: implement getAllMatchDynamicsInfo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<MatchDynamicsModel> getMatchDynamicsInfoById(int id) {
+    // TODO: implement getMatchDynamicsInfoById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<MatchDynamicsModel> insertMatchDynamicInfo(
+      String title, String description) {
+    // TODO: implement insertMatchDynamicInfo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<MatchDynamicsModel> updateMatchDynamicInfo(
+      MatchDynamicsModel matchDynamic) {
+    // TODO: implement updateMatchDynamicInfo
+    throw UnimplementedError();
+  }
 }
--- test/features/mentor_center/data/datasources/match_dynamics_local_data_source_test.dart
+++ test/features/mentor_center/data/datasources/match_dynamics_local_data_source_test.dart
@@ -1,3 +1,74 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:virtual_tennis_mentor/features/mentor_center/data/datasources/match_dynamics_local_datasource.dart';
+
+void main() {
+  MatchDynamicsLocalDataSource dataSource;
+
+  group(
+    'get database',
+    () {
+      test(
+        'should return passed instance of database',
+        () async {
+          // arrange
+          final String expected =
+              'sqflite_common_ffi\\databases\\match_dynamics_stub_db\\match_dynamics.db';
+          if (Platform.isWindows) {
+            sqfliteFfiInit();
+          }
+          databaseFactory = databaseFactoryFfi;
+          final database =
+              await openDatabase('match_dynamics_stub_db\\match_dynamics.db');
+          MatchDynamicsLocalDatasourceImpl dataSourceImpl =
+              MatchDynamicsLocalDatasourceImpl.injectDatabase(database);
+
+          // act
+          final result = await dataSourceImpl.database;
+
+          // assert
+          expect(result.path, endsWith(expected));
+        },
+      );
+
+      test(
+        'should return database from assets if null passed in constructor',
+        () async {
+          // arrange
+
+          // act
+          MatchDynamicsLocalDatasourceImpl dataSourceImpl =
+              MatchDynamicsLocalDatasourceImpl.instance;
+          dataSourceImpl.database;
+          // assert
+          expect(true, false);
+        },
+      );
+    },
+  );
+
+  group(
+    'database operations',
+    () {
+      setUp(() {});
+      group('add to db', () {
+        test(
+          'should ',
+          () async {
+            // arrange
+
+            // act
+
+            // assert
+            expect(true, false);
+          },
+        );
+      });
+    },
+  );
+}
